393502053da29615c5b6e5d3b027ad1405a781c9
Andrey Kishtov <andreykishtov@gmail.com>
2018-03-06 19:25:06 +0200

    simple express server with axios in react sending data

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 var express = require('express');
+var cors = require('cors');
 var app = express();
+var bodyParser = require('body-parser');
 
 var port = '4000';
 
-app.get('/', (req, res) => res.json('works')).post('/', (req, res) => res.json('post'));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.get('/', (req, res) => res.json('works')).post('/', (req, res) => res.json(req.body));
 
 app.listen(port, () => console.log(`works on ${port}`));
